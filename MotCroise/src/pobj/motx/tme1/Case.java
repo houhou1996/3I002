72ee185656b51ef93670a40b9103a3396f52c529
Tamene hocine <tamenehocine2012@gmail?com>
2019-01-23 20:48:29 +0100

    CaseTest

---
+++ MotCroise/src/pobj/motx/tme1/Case.java
@@ -0,0 +1,36 @@
+package pobj.motx.tme1;
+
+public class Case {
+	private int lig,col;
+	private char c;
+	
+public Case(int lig, int col, char c) {
+	this.lig = lig;
+	this.col = col;
+	this.c = c;
+}
+
+public char getChar() {
+	return c;
+}
+
+public void setChar(char c) {
+	this.c = c;
+}
+
+public int getLig() {
+	return lig;
+}
+
+public int getCol() {
+	return col;
+}
+public boolean isVide() {
+	return c == ' ';
+}
+public boolean isPleine() {
+	return c == '*';
+}
+
+
+}
